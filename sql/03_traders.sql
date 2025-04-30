c18dafd1be3854300e61e81bafec7a5a3c561cde
Samuel Bamidele <40490956+SamBami01@users.noreply.github.com>
2025-04-30 14:58:28 +0100

    Update 03_traders.sql

--- sql/03_traders.sql
+++ sql/03_traders.sql
@@ -4,10 +4,8 @@ WITH unique_wallets AS (
     contract_address AS token_address,
     "from" AS wallet
   FROM erc20_ethereum.evt_transfer
-  WHERE contract_address IN (
-    0x6982508145454ce325ddbe47a25d4ec3d2311933, -- PEPE
-    0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce  -- SHIB
-  )
+  
+  WHERE contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xcf0c122c6b73ff809c693db761e7baebe62b6a2e /* FLOKI */,  0xaaee1a9723aadb7afa2810263653a34ba2c21c7a /* MOG */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
 
   UNION
 
@@ -16,10 +14,8 @@ WITH unique_wallets AS (
     contract_address AS token_address,
     "to" AS wallet
   FROM erc20_ethereum.evt_transfer
-  WHERE contract_address IN (
-    0x6982508145454ce325ddbe47a25d4ec3d2311933,
-    0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce
-  )
+  WHERE contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xcf0c122c6b73ff809c693db761e7baebe62b6a2e /* FLOKI */,  0xaaee1a9723aadb7afa2810263653a34ba2c21c7a /* MOG */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
+
 )
 
 SELECT
