65eeab5db869f1cb5b591f5090efa811887f95dd
Samuel Bamidele <40490956+SamBami01@users.noreply.github.com>
2025-04-30 14:16:30 +0100

    Update 01_volume.sql

--- sql/01_volume.sql
+++ sql/01_volume.sql
@@ -12,7 +12,7 @@ WITH transfers AS (
   LEFT JOIN tokens.erc20 AS m
     ON t.contract_address = m.contract_address
   WHERE
-    t.contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
+    t.contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xfb5b838b6cfeedc2873ab27866079ac55363d37e /* FLOKI */,  0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f /* SAFE */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
   GROUP BY
     1,
     2
