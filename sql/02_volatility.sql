5d2996a554624ec13708220ebe903457bb4be50f
Samuel Bamidele <40490956+SamBami01@users.noreply.github.com>
2025-04-30 14:14:07 +0100

    Update 02_volatility.sql

--- sql/02_volatility.sql
+++ sql/02_volatility.sql
@@ -4,13 +4,12 @@ WITH daily_prices AS (
     DATE_TRUNC('day', p.minute) AS day,
     AVG(p.price) AS avg_price
   FROM prices.usd AS p
-  WHERE p.contract_address IN (
-    0x6982508145454ce325ddbe47a25d4ec3d2311933, -- PEPE
-    0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce  -- SHIB
-  )
-  GROUP BY 1, 2
-),
-log_returns AS (
+  WHERE
+    p.contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xfb5b838b6cfeedc2873ab27866079ac55363d37e /* FLOKI */,  0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f /* SAFE */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
+  GROUP BY
+    1,
+    2
+), log_returns AS (
   SELECT
     token_address,
     day,
@@ -20,11 +19,7 @@ log_returns AS (
 SELECT
   token_address,
   day,
-  STDDEV_SAMP(log_return)
-    OVER (
-      PARTITION BY token_address
-      ORDER BY day
-      ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ) AS vol_7d
+  STDDEV_SAMP(log_return) OVER (PARTITION BY token_address ORDER BY day ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS vol_7d
 FROM log_returns
-ORDER BY day desc
+ORDER BY
+  day DESC
