83ae4ceb2da605bed81f3a3618612e858e2966f0
Samuel Bamidele <40490956+SamBami01@users.noreply.github.com>
2025-05-01 16:45:45 +0100

    Update 03_traders.sql

--- sql/03_traders.sql
+++ sql/03_traders.sql
@@ -1,27 +1,34 @@
-WITH unique_wallets AS (
-  SELECT
-    DATE_TRUNC('day', evt_block_time) AS day,
-    contract_address AS token_address,
-    "from" AS wallet
-  FROM erc20_ethereum.evt_transfer
-  
-  WHERE contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xcf0c122c6b73ff809c693db761e7baebe62b6a2e /* FLOKI */,  0xaaee1a9723aadb7afa2810263653a34ba2c21c7a /* MOG */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
-
-  UNION
-
-  SELECT
-    DATE_TRUNC('day', evt_block_time) AS day,
-    contract_address AS token_address,
-    "to" AS wallet
-  FROM erc20_ethereum.evt_transfer
-  WHERE contract_address IN (0x6982508145454ce325ddbe47a25d4ec3d2311933 /* PEPE */, 0xcf0c122c6b73ff809c693db761e7baebe62b6a2e /* FLOKI */,  0xaaee1a9723aadb7afa2810263653a34ba2c21c7a /* MOG */, 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce /* SHIB */)
-
-)
-
-SELECT
-  day,
-  token_address,
-  COUNT(DISTINCT wallet) AS unique_traders
-FROM unique_wallets
-GROUP BY day, token_address
-ORDER BY day DESC, token_address
+with wallets as (
+    select
+        DATE_TRUNC('day', evt_block_time) as day
+        , contract_address
+        , "from" as wallet
+        from erc20_ethereum.evt_transfer
+        where CAST(contract_address as VARCHAR) in (
+                select
+                    *
+                from unnest (split('{{token_list}}', ',')) as token_address)
+        union
+        select
+    DATE_TRUNC('day', evt_block_time)
+    , contract_address
+    , "to"
+        from erc20_ethereum.evt_transfer
+        where CAST(contract_address as VARCHAR) in (
+                select
+                    *
+                from unnest (split('{{token_list}}', ',')) as token_address))
+        , traders as (
+        select
+        day
+        , contract_address
+        , COUNT(distinct wallet) as unique_traders
+        from wallets
+        group by
+    1
+    , 2)
+select
+    *
+from traders
+order by
+    day desc
